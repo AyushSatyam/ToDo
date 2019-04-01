1016c01d86844da17793bd4b11a75af6c22c8b59
AyushSatyam <42187810+AyushSatyam@users.noreply.github.com>
2019-04-02 00:13:50 +0530

    ToDo web app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import TodoList from "./TodoList";
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+  <div class>
+    <TodoList />
+  </div>,
+  destination
+);
